cd4fb93f3073e94eb8a95ede5e0d82a5fc68bf6f
Krish Koria <krishkoria2004@gmail.com>
2025-11-10 02:02:38 +0530

    feat: add node selector and execution nodes for manual and HTTP requests

---
+++ src/app/(dashboard)/(editor)/_components/nodes/triggers/manual-trigger.tsx/node.tsx
@@ -0,0 +1,19 @@
+import type { NodeProps } from "@xyflow/react";
+import { memo } from "react";
+import { BaseTriggerNode } from "../base-trigger-node";
+import { MousePointerIcon } from "lucide-react";
+
+export const ManualTriggerNode = memo((props: NodeProps) => {
+  return (
+    <>
+      <BaseTriggerNode
+        {...props}
+        id={props.id}
+        icon={MousePointerIcon}
+        name={"When Clicking 'Execute Workflow' "}
+        onSettings={() => {}}
+        onDoubleClick={() => {}}
+      />
+    </>
+  );
+});
--- src/config/node-components.ts
+++ src/config/node-components.ts
@@ -1,9 +1,13 @@
+import { HttpRequestNode } from "@/app/(dashboard)/(editor)/_components/nodes/executions/http-request/node";
 import { InitialNode } from "@/app/(dashboard)/(editor)/_components/nodes/initialNode";
+import { ManualTriggerNode } from "@/app/(dashboard)/(editor)/_components/nodes/triggers/manual-trigger.tsx/node";
 import { NodeType } from "@/server/db/schema";
 import type { NodeTypes } from "@xyflow/react";
 
 export const nodeComponents = {
   [NodeType.INITIAL]: InitialNode,
+  [NodeType.MANUAL_TRIGGER]: ManualTriggerNode,
+  [NodeType.HTTP_REQUEST]: HttpRequestNode,
 } as const satisfies NodeTypes;
 
 export type RegisteredNodeType = keyof typeof nodeComponents;
